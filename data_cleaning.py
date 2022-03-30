fa9fe12c0ba81de0f0c8aa541f17fdfa442aa3fc
Keith Allatt <keith.allatt@gmail.com>
2022-03-30 14:32:31 -0400

    Changed Data cleaner to generator. No longer saves pickle files.
    Updated gitignore to not track dataset folder.

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,9 +5,8 @@ Load the data, extract the relevant information from the csv files to create a m
 
 Author(s): Keith Allatt,
 """
-from typing import Union, Tuple
+from typing import Union, Generator
 
-import utilities
 from pathlib import Path
 import csv
 
@@ -18,27 +17,29 @@ validation_data = Path("./dbpedia_data/DBPEDIA_val.csv")
 testing_data = Path("./dbpedia_data/DBPEDIA_test.csv")
 
 
-def clean_csv_into_pickle(infile: Union[str, Path], outfile: Union[str, Path] = None) -> Tuple[list, list, list]:
-    """ Clean csv file into a pickle file for future use """
-    if outfile is None:
-        # infer outfile path
-        outfile = infile.with_name(infile.stem + "_cleaned.pickle")
+def csv_file_generator(infile: Union[str, Path]) -> Generator[tuple, None, None]:
+    """ Clean csv file contents and yield row by row. """
 
     l1i, l2i, l3i = 1, 2, 3  # indices of l1, l2, and l3 category in all data file.
-    l1_category, l2_category, l3_category = [], [], []  # extract labels for each
-    cleaned_set = []
+
+    # was useful for testing that the categories were being extracted properly from the csv file.
+    # l1_category, l2_category, l3_category = [], [], []  # extract labels for each
 
     with open(infile, mode='r') as file:
+        # reads file in, line by line.
         csv_file = csv.reader(file)
         for line in csv_file:
+            # first row has the header, can disregard.
             if line[0] == "text":
                 continue
+
             # remove unnecessary empty strings from end of list
             line = list(filter(lambda x: x, line))
+
             # how many of the first cells are actually the summary split by semicolons?
             num_groups = len(line) - 5  # if no semi's, only one, and thus it is good.
-            summary = ";".join(line[:num_groups])
-            line = [summary] + line[num_groups:]
+            doc = ";".join(line[:num_groups])
+            line = [doc] + line[num_groups:]
 
             # get l1, l2, and l3 category
             l1 = line[l1i]
@@ -46,26 +47,24 @@ def clean_csv_into_pickle(infile: Union[str, Path], outfile: Union[str, Path] = 
             l3 = line[l3i]
 
             # in all examples, these are the categories that are misplaced in l1
-            # when semicolons appear in the article name.
+            # when semicolons appear in the article name. This will revert the
+            # change made when the article name gets split in two and thus shifts the categories.
             if l1 in ["MusicalWork", "Comic", "Cartoon", "AmusementParkAttraction", "Software"]:
-                l3 = l2
-                l2 = l1
+                # shift l1-l2 to l2-l3,
+                l2, l3 = l1, l2
                 # get the l1 cat from summary
-                l1 = summary.split(";")[-1]
+                l1 = doc.split(";")[-1]
                 # and remove it from summary
-                summary = ";".join(summary.split(";")[:-1])
+                doc = ";".join(doc.split(";")[:-1])
 
-            for l, lc in [(l1, l1_category), (l2, l2_category), (l3, l3_category)]:
-                if l not in lc:
-                    lc.append(l)
+            # for ll, lc in [(l1, l1_category), (l2, l2_category), (l3, l3_category)]:
+            #     if ll not in lc:
+            #         lc.append(ll)
 
-            cleaned_set.append((summary, l1, l2, l3))
-
-    utilities.dump_data(outfile, cleaned_set)
-    return l1_category, l2_category, l3_category
+            yield doc, (l1, l2, l3)
 
 
 if __name__ == '__main__':
-    l1c, l2c, l3c = clean_csv_into_pickle(all_data)
-    assert list(map(len, [l1c, l2c, l3c])) == [9, 70, 219], "Categories have wrong number of elements."
-
+    g = csv_file_generator(all_data)
+    for d, ls in g:
+        print(d[:min(len(d), 100)], ls)
