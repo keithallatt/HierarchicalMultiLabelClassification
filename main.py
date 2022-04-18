aa796a19525dbd0fc9ccee667a080d5e5ac39273
Keith Allatt <keith.allatt@gmail.com>
2022-04-18 19:52:30 -0400

    Added successful and unsuccessful classifications.

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ if __name__ == "__main__":
     small_file_fmt = "processed_data/DBPEDIA_{split}_small_{var}.pt"
     l2_l1_file_fmt = "processed_data/DBPEDIA_l2_l1_Agent_{var}.pt"
 
-
     # how much data to load
     train_obs = 40000
     val_obs = 36003
@@ -36,21 +35,17 @@ if __name__ == "__main__":
     # train = DBPedia(l2_l1_file_fmt.format(var="embeddings"),
     #                 l2_l1_file_fmt.format(var="labels"),
     #                 obs=train_obs)
-       
-    
+
     train = DBPedia(file_fmt.format(split="train", var="embeddings"),
-                        file_fmt.format(split="train", var="labels"),
-                        obs=train_obs)
-
-
+                    file_fmt.format(split="train", var="labels"),
+                    obs=train_obs)
     val = DBPedia(file_fmt.format(split="val", var="embeddings"),
-                    file_fmt.format(split="val", var="labels"),
-                    obs=val_obs)
+                  file_fmt.format(split="val", var="labels"),
+                  obs=val_obs)
     test = DBPedia(file_fmt.format(split="test", var="embeddings"),
-                    file_fmt.format(split="test", var="labels"),
-                    obs=test_obs)
+                   file_fmt.format(split="test", var="labels"),
+                   obs=test_obs)
 
-    
     # uncomment model to use
 
     model = HierarchicalRNN(
@@ -64,7 +59,8 @@ if __name__ == "__main__":
     '''
     Model Checkpointing Notes
     
-    For checkpointing/saving a model, set checkpoint_path to the directory for model checkpoint to be saved i.e. ./checkpoint/
+    For checkpointing/saving a model, set checkpoint_path to the directory for 
+    model checkpoint to be saved i.e. "./checkpoint/".
 
     To load a checkpointed model and train on it set: load_checkpoint: True and 
     load_checkpoint_path = <path-to-checkpointed-model> ex. "./checkpoint/model_233939_0"
@@ -81,11 +77,11 @@ if __name__ == "__main__":
         # "load_checkpoint": True,
         # "load_checkpoint_path": './checkpoints/18-04-2022 11:19:48/model_18-04-2022 11:19:48_20',
         "optimizer": "adam",
-        "tf_init": 0, 
+        "tf_init": 0,
         "tf_decay": 0.5
     }
 
-    #param_sizes = get_param_sizes(model)
+    # param_sizes = get_param_sizes(model)
 
     '''
     Toggle save_imgs to True to save imgs to an imgs directory which will be created if it doesn't exist: imgs/
@@ -94,13 +90,21 @@ if __name__ == "__main__":
     ho = {'calc_acc_every': 4, 'batch_size': 64, 'learning_rate': 0.001020977066089074,
           'weight_decay': 0.000, 'momentum': 0.000, 'num_epochs': 14}
     train_opts.update(ho)
-    
+
     # train_model(model, train, val, test,
     #             device=device, train_opts=train_opts, show_plts=False, save_imgs=False)
 
     # needed to load pickle file.
-    # from data_cleaning import WordIdMapping
-    # data_mapping = pickle.load(open('./processed_data/mapping.pkl', 'rb'))
-    # find_correct_classifications(model, opts=train_opts, device=device, word_mapping=data_mapping)
 
 
+
+    from data_cleaning import WordIdMapping
+
+    options = {
+        'load_checkpoint': True,
+        'load_checkpoint_path': "./checkpoints/18-04-2022 16:20:40/model_18-04-2022 16:20:40_14"
+    }
+
+    data_mapping = pickle.load(open('./processed_data/mapping.pkl', 'rb'))
+    res = find_correct_classifications(model, opts=options, device=device, word_mapping=data_mapping)
+    print(res)
